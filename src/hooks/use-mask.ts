09b789ea85eff10526253c74614ba595fd7e05c8
fgc17 <fgcoelho1709@outlook.com>
2024-09-14 19:21:01 +0000

    feat: use input mask hook

--- src/hooks/use-mask.ts
+++ src/hooks/use-mask.ts
@@ -23,26 +23,22 @@ export const maskit = (string: string, mask: MaskType) => {
     const inputChar = rawValue[inputIndex];
 
     if (maskChar === "9") {
-      // Placeholder for a digit
       if (!/\d/.test(inputChar || "")) {
-        break; // Break if input character is not a digit
+        break;
       }
       formattedValue += inputChar;
       inputIndex++;
     } else if (maskChar === "a") {
-      // Placeholder for a letter
       if (!/[a-zA-Z]/.test(inputChar || "")) {
-        break; // Break if input character is not a letter
+        break;
       }
       formattedValue += inputChar;
       inputIndex++;
     } else if (maskChar === "*") {
-      // Wildcard character, accept any character
       formattedValue += inputChar;
       inputIndex++;
     } else {
       formattedValue += maskChar;
-      // No need to check if inputChar matches maskChar, as rawValue contains only letters and digits
     }
     maskIndex++;
   }
