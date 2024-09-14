d746e6e65bccf8a5cc58bde945fe42a685c8f64e
fgc17 <fgcoelho1709@outlook.com>
2024-09-14 19:25:55 +0000

    feat(button): react19, loading, headlessui

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
+import * as Headless from "@headlessui/react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn, InnerRef } from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { Spinner } from "./spinner";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
   {
     variants: {
       variant: {
@@ -24,6 +26,11 @@ const buttonVariants = cva(
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        variable: "px-4 py-2",
+      },
+      loading: {
+        true: "cursor-wait flex items-center gap-2",
+        false: "",
       },
     },
     defaultVariants: {
@@ -33,9 +40,11 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+interface ButtonProps
+  extends Headless.ButtonProps,
     VariantProps<typeof buttonVariants> {
+  children?: React.ReactNode;
+  ref?: React.Ref<HTMLButtonElement>;
   asChild?: boolean;
   loading?: boolean;
 }
@@ -44,32 +53,33 @@ function Button({
   className,
   variant,
   size,
+  children,
   asChild = false,
-  innerRef,
+  loading,
   ...props
-}: InnerRef<ButtonProps>) {
-  const Comp = asChild ? Slot : "button";
-  return (
-    <TouchTarget>
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={innerRef}
+}: ButtonProps) {
+  const cname = cn(buttonVariants({ variant, size, className }));
+
+  if (asChild) {
+    return (
+      <Slot
+        className={cname}
+        children={children as React.ReactNode}
         {...props}
       />
-    </TouchTarget>
-  );
-}
+    );
+  }
 
-function TouchTarget({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {children}
-      <span
-        className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
-        aria-hidden="true"
-      />
-    </>
+    <Headless.Button
+      className={cn(buttonVariants({ variant, loading, size, className }))}
+      disabled={props.disabled || loading}
+      {...props}
+    >
+      {children as React.ReactNode}
+      <Spinner size={"small"} show={loading} />
+    </Headless.Button>
   );
 }
 
-export { Button, TouchTarget, buttonVariants };
+export { type ButtonProps, Button, buttonVariants };
