a7f5909e81bc967f679b328a2d183ea1baa4074c
fgc17 <fgcoelho1709@outlook.com>
2024-09-15 16:51:44 +0000

    developing: form-group

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,37 @@ export type BagChildren<T> = Omit<T, "children"> & {
   children?: React.ReactNode;
 };
 
+export interface MapObject<Props extends { [key: string]: any }> {
+  get<K extends keyof Props>(key: K): Props[K];
+
+  set<K extends keyof Props>(key: K, value: Props[K]): this;
+
+  delete<K extends keyof Props>(key: K): boolean;
+
+  has<K extends keyof Props>(key: K): boolean;
+
+  forEach(
+    callbackfn: (
+      value: Props[keyof Props],
+      key: keyof Props,
+      map: MapObject<Props>
+    ) => void,
+    thisArg?: any
+  ): void;
+
+  entries(): IterableIterator<[keyof Props, Props[keyof Props]]>;
+
+  keys(): IterableIterator<keyof Props>;
+
+  values(): IterableIterator<Props[keyof Props]>;
+
+  [Symbol.iterator](): IterableIterator<[keyof Props, Props[keyof Props]]>;
+
+  get size(): number;
+
+  clear(): void;
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
