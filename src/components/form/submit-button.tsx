43048d9e36832f3f539f68a3864a3ba8a19126b6
fgc17 <fgcoelho1709@outlook.com>
2024-09-22 03:18:38 +0000

    fix: optional hform prop

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -5,19 +5,14 @@ import { useParentForm } from "./form";
 
 export function SubmitButton(
   props: ButtonProps & {
-    hform: { formState: { isSubmitting: boolean }; id: string };
+    hform?: { formState: { isSubmitting: boolean }; id: string };
   }
 ) {
   const _form = useParentForm();
 
   const form = props.hform ?? _form;
 
-  return (
-    <Button
-      {...props}
-      loading={form.formState.isSubmitting ? true : false}
-      form={form.id}
-      type="submit"
-    />
-  );
+  const isLoading = form?.formState?.isSubmitting;
+
+  return <Button {...props} loading={isLoading} form={form.id} type="submit" />;
 }
