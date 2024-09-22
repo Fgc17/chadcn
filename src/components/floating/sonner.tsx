2b9d026d2e9e42674328ca04095d3c5b5c65929a
fgc17 <fgcoelho1709@outlook.com>
2024-09-22 03:14:20 +0000

    feat: error sonner toast

--- src/components/floating/sonner.tsx
+++ src/components/floating/sonner.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
@@ -16,7 +16,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-950 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-gray-950 dark:group-[.toaster]:text-gray-50 dark:group-[.toaster]:border-gray-800",
-          description: "group-[.toast]:text-gray-500 dark:group-[.toast]:text-gray-400",
+          error:
+            "group toast group-[.toaster]:bg-red-50 group-[.toaster]:text-red-900 dark:group-[.toaster]:bg-red-900 dark:group-[.toaster]:text-red-50",
+          description:
+            "group-[.toast]:text-gray-500 dark:group-[.toast]:text-gray-400",
           actionButton:
             "group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50 dark:group-[.toast]:bg-gray-50 dark:group-[.toast]:text-gray-900",
           cancelButton:
@@ -25,7 +28,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { toast } from "sonner";
+
+export { Toaster };
