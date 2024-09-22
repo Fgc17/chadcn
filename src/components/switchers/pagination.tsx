1ada260e610557267b6066d0834fb5402b5e5c0b
fgc17 <fgcoelho1709@outlook.com>
2024-09-22 03:14:49 +0000

    feat: pagination rework

--- src/components/switchers/pagination.tsx
+++ src/components/switchers/pagination.tsx
@@ -1,115 +1,134 @@
-import * as React from "react";
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { ButtonProps, buttonVariants } from "@/components/button";
+import clsx from "clsx";
+import type React from "react";
+import { Button } from "../button";
+import Link from "next/link";
+import { Icon } from "../icon";
 
-const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
-  <nav
-    role="navigation"
-    aria-label="pagination"
-    className={cn("mx-auto flex w-full justify-center", className)}
-    {...props}
-  />
-);
-
-function PaginationContent({
+export function Pagination({
+  "aria-label": ariaLabel = "Page navigation",
   className,
   ...props
-}: React.ComponentProps<"ul">) {
+}: React.ComponentPropsWithoutRef<"nav">) {
   return (
-    <ul
-      className={cn("flex flex-row items-center gap-1", className)}
+    <nav
+      aria-label={ariaLabel}
       {...props}
+      className={clsx(className, "flex items-center")}
     />
   );
 }
 
-function PaginationItem({ className, ...props }: React.ComponentProps<"li">) {
-  return <li className={cn("", className)} {...props} />;
-}
-
-type PaginationLinkProps = {
-  isActive?: boolean;
-} & Pick<ButtonProps, "size"> &
-  React.ComponentProps<"a">;
-
-function PaginationLink({
-  className,
-  isActive,
-  size = "icon",
-  ...props
-}: PaginationLinkProps) {
+export function PaginationPrevious({
+  href,
+  children = "Previous",
+  onClick,
+  disabled = false,
+}: {
+  onClick?: () => void;
+  href?: string;
+  children?: React.ReactNode;
+  disabled?: boolean;
+}) {
   return (
-    <a
-      aria-current={isActive ? "page" : undefined}
-      className={cn(
-        buttonVariants({
-          variant: isActive ? "outline" : "ghost",
-          size,
-        }),
-        className
-      )}
-      {...props}
-    />
-  );
-}
-
-function PaginationPrevious({
-  className,
-  ...props
-}: React.ComponentProps<typeof PaginationLink>) {
-  return (
-    <PaginationLink
-      aria-label="Go to previous page"
-      size="default"
-      className={cn("gap-1 pl-2.5", className)}
-      {...props}
-    >
-      <ChevronLeft className="h-4 w-4" />
-      <span>Previous</span>
-    </PaginationLink>
-  );
-}
-
-function PaginationNext({
-  className,
-  ...props
-}: React.ComponentProps<typeof PaginationLink>) {
-  return (
-    <PaginationLink
-      aria-label="Go to next page"
-      size="default"
-      className={cn("gap-1 pr-2.5", className)}
-      {...props}
-    >
-      <span>Next</span>
-      <ChevronRight className="h-4 w-4" />
-    </PaginationLink>
-  );
-}
-
-function PaginationEllipsis({
-  className,
-  ...props
-}: React.ComponentProps<"span">) {
-  return (
-    <span
-      aria-hidden
-      className={cn("flex h-9 w-9 items-center justify-center", className)}
-      {...props}
-    >
-      <MoreHorizontal className="h-4 w-4" />
-      <span className="sr-only">More pages</span>
+    <span className="grow basis-0">
+      <Link href={href ?? ""}>
+        <Button
+          onClick={onClick}
+          variant="plain"
+          disabled={disabled}
+          aria-label="Previous page"
+          className="text-sm"
+        >
+          <Icon name="ArrowLeft" className="w-4" />
+          {children}
+        </Button>
+      </Link>
     </span>
   );
 }
 
-export {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-};
+export function PaginationNext({
+  href,
+  children = "Next",
+  onClick,
+  disabled = false,
+}: {
+  onClick?: () => void;
+  disabled?: boolean;
+  href?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <span className="flex grow basis-0 justify-end">
+      <Link href={href ?? ""}>
+        <Button
+          onClick={onClick}
+          variant="plain"
+          disabled={disabled}
+          aria-label="Next page"
+          className="text-sm"
+        >
+          {children}
+          <Icon name="ArrowRight" className="w-4" />
+        </Button>
+      </Link>
+    </span>
+  );
+}
+
+export function PaginationList({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <span className={clsx("flex items-baseline *:text-xs", className)}>
+      {children}
+    </span>
+  );
+}
+
+export function PaginationPage({
+  href,
+  children,
+  current = false,
+  onClick,
+  className,
+}: {
+  href?: string;
+  children: string;
+  current?: boolean;
+  onClick?: () => void;
+  className?: string;
+}) {
+  return (
+    <Link href={href ?? ""}>
+      <Button
+        variant="plain"
+        aria-label={`Page ${children}`}
+        aria-current={current ? "page" : undefined}
+        onClick={onClick}
+        className={clsx(
+          "min-w-[2.25rem] text-sm before:absolute before:-inset-px before:rounded-lg",
+          current && "before:bg-zinc-950/5",
+          className
+        )}
+      >
+        <span className="-mx-0.5">{children}</span>
+      </Button>
+    </Link>
+  );
+}
+
+export function PaginationGap() {
+  return (
+    <div
+      aria-hidden="true"
+      className="w-[2.25rem] select-none text-center text-sm/6 font-semibold text-zinc-950"
+    >
+      &hellip;
+    </div>
+  );
+}
